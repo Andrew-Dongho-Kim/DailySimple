b29cdf26c1a0c70b361dd08869f1e9aa6697f40e
Andrew-Dongho-Kim <59630591+Andrew-Dongho-Kim@users.noreply.github.com>
2020-03-08 19:45:59 +0900

    Skeleton codes for app (room, recycler view, coordinator layout)
    Implementation skeleton codes
      - Database ; Schedule (with Room)
      - CoordinatorLayout
      - RecyclerView

---
+++ app/src/main/java/com/dd/android/dailysimple/schedule/detail/ScheduleDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.dd.android.dailysimple.schedule.detail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ScheduleDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
---
+++ app/src/main/java/com/dd/android/dailysimple/schedule/detail/ScheduleDetailFragment.kt
@@ -0,0 +1,26 @@
+package com.dd.android.dailysimple.schedule.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.dd.android.dailysimple.R
+
+class ScheduleDetailFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View =
+        LayoutInflater.from(context).inflate(R.layout.activity_schedule_detail, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+    
+
+    companion object {
+        const val TAG = "ScheduleDetailFragment"
+    }
+}
